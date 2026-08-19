68967e133cf0461dfb4c0c8a2921710f0fec82a2
wycliffe36 <73503275+wycliffe36@users.noreply.github.com>
2026-08-19 14:26:43 +0300

    Update etl.py

--- 03_etl/03_etl/etl.py
+++ 03_etl/03_etl/etl.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-def process_large_csv(filepath):
-    df = pd.read_csv(filepath)
-    result = df.groupby('category')['amount'].sum()
-    return result
+def transform_csv(input_path: str, output_path: str):
+    chunks = []
+    for chunk in pd.read_csv(input_path, chunksize=100000):
+        chunk["total"] = chunk["price"] * chunk["quantity"]
+        chunks.append(chunk)
+    df = pd.concat(chunks)
+    df.to_csv(output_path, index=False)
